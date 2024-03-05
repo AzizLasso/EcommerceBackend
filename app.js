2b1b998e53c0b7fdeb34edd2c20ed7d103ab53d5
AzizLasso <azizlassouedla@gmail.com>
2024-03-05 10:16:01 +0100

    Commit new version

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const cors = require('cors')
 const app = express();
 const categorieRouter =require("./routes/categorie.route")
 app.use('/api/categories', categorieRouter);
+const scategorieRouter =require("./routes/scategorie.route")
+app.use('/api/scategories', scategorieRouter);
+const articleRouter =require("./routes/article.route")
+app.use('/api/articles', articleRouter);
 //config dotenv
 dotenv.config()
 //Les cors
